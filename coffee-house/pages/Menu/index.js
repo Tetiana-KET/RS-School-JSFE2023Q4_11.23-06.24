aebd21f4d819e95a593b8ee11923ab1efba66a7c
tatiana <belangelphone@gmail.com>
2023-11-19 11:25:42 +0200

    build: add main project structure create folder-structure, add favicone, defined the colors-variables, linked the fonts

---
+++ coffee-house/pages/Menu/index.js
@@ -0,0 +1,21 @@
+import './normalize.css';
+import './style.css';
+// import minesweeper from './assets/images/minesweeper.jpg';
+
+// const hello = require('./hello');
+
+// const imgMine = document.getElementById('imgId');
+// imgMine.src = minesweeper;
+
+/**
+ * burger
+ * document.addEventListener('click', toggleBurgerMenu);
+
+  function toggleBurgerMenu(e) {
+    const targetItem = e.target;
+
+    if(targetItem.closest('.burger-menu')) {
+      document.documentElement.classList.toggle('menu-open');
+    }
+  }
+ */
