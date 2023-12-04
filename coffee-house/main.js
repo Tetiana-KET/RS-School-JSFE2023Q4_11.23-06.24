18047d8a7e006b98603c0b20f688a5485787e946
tatiana <belangelphone@gmail.com>
2023-12-04 14:49:58 +0200

    feat: Implement the burger menu on both pages

---
+++ coffee-house/main.js
@@ -0,0 +1,56 @@
+"use strict"
+window.addEventListener('DOMContentLoaded', () => {
+
+  const body = document.body;
+  const burgerButton = document.querySelector('.header__burger');
+  const headerBurgerMenu = document.querySelector('.header__nav');
+  const overlay = document.querySelector('.overlay');
+
+
+
+	function lockBodyScroll() {
+		body.classList.add('no-scroll');
+	}
+
+	function unlockBodyScroll() {
+		body.classList.remove('no-scroll');
+	}
+
+  function toggleHeaderMenu () {
+
+		if (burgerButton.classList.contains('menu-open')) {
+
+			headerBurgerMenu.classList.remove('header__nav_open');
+			burgerButton.classList.remove('menu-open');
+			// overlay.classList.remove('active-overlay');
+
+			unlockBodyScroll();
+
+		} else {
+
+			headerBurgerMenu.classList.add('header__nav_open');
+			burgerButton.classList.add('menu-open');
+			// overlay.classList.add('active-overlay');
+			lockBodyScroll();
+		}
+	}
+
+  
+  body.addEventListener('click', (e) => {
+
+		if (window.screen.width <= 768) {
+			if (
+				e.target.classList.contains('burger') ||
+				e.target.classList.contains('burger__line') ||
+				e.target.classList.contains('nav__link') ||
+				e.target.classList.contains('menu__link')
+			) {
+				e.stopPropagation();
+				toggleHeaderMenu();
+			}
+		}
+
+    
+		
+  });
+})
