1018e18e79e3024d65cd7819233e231e9d887242
tatiana <belangelphone@gmail.com>
2024-01-22 20:03:44 +0200

    feat: add header

---
+++ nonograms/src/headerTemplate.js
@@ -0,0 +1,13 @@
+export default function createHeaderHtml() {
+  return `
+    <div class="header___container">
+      <div class="settings-btns-wrap">
+        <button class="button buttons-settings">New game</button>
+        <button class="button buttons-settings">Last game</button>
+        <button class="button buttons-settings">Reset game</button>
+        <button class="button buttons-settings">Solution</button>
+        <button class="button buttons-settings">Random</button>
+      </div>
+  </div>
+  `;
+}
--- nonograms/src/htmlGenerator.js
+++ nonograms/src/htmlGenerator.js
@@ -1,16 +1,30 @@
+import createFooterHtml from './footerTemplate';
+import createHeaderHtml from './headerTemplate';
+
 export default function generateHtml() {
-	console.log('Hello world!');
-	const page = document.createElement('main');
-	page.classList.add('page');
-	document.body.prepend(page);
+
+	const siteWrapper = document.createElement('div');
+	siteWrapper.classList.add('site-wrapper');
+	document.body.prepend(siteWrapper);
 
 	// container
 	const container = document.createElement('div');
 	container.classList.add('container');
-	page.prepend(container);
 
-	const header = document.createElement('div');
+	const header = document.createElement('header');
 	header.classList.add('header');
-	container.prepend(header);
-	header.textContent = 'Проверка связи';
+	siteWrapper.prepend(header);
+	header.innerHTML = createHeaderHtml();
+
+	const main = document.createElement('main');
+	main.classList.add('main');
+	siteWrapper.append(main);
+	main.textContent = 'main';
+
+	const footer = document.createElement('footer');
+	footer.classList.add('footer');
+	siteWrapper.append(footer);
+	footer.innerHTML = createFooterHtml();
+
+
 }
