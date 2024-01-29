1ae069914d245469dfe83fb7ce8c094a3000679c
tatiana <belangelphone@gmail.com>
2024-01-29 16:33:11 +0200

    refactor: improve UI replace select puzzle name button to the block with settings at the top of the page and button switcher__theme-name to the bottom

--- nonograms/src/components/infoTemplate.js
+++ nonograms/src/components/infoTemplate.js
@@ -5,23 +5,17 @@ export default function createInfoHtml() {
         <li class="info__item">
           <button type="button" class="button info__score"></button>
         </li>
-
-        <li class="info__item select__hint hint">
-            <div class="hint__wrap">
-            <div class="hint__header">
-              <div class="hint__header-content">
-                <span class="hint__name">Select Image</span>
-                <span class="hint__icon">
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="8" cy="8" r="8" fill="#e8cba8"></circle>
-                    <path d="M8.00003 8.99997L6.00002 7.00008" stroke="#5d4132ed" stroke-width="0.5" stroke-linecap="round"></path>
-                    <path d="M8 9L10 7" stroke="#5d4132ed" stroke-width="1" stroke-linecap="round"></path>
-                  </svg>
-                </span>
-              </div>
-            </div>
-            <ul class="hint__body select-options"></ul>
-          </div>
+        <li class="info__switcher switcher">
+          <button class="button switcher__theme-name">
+            Dark
+          </button>
+          <label class="switcher__button">
+            <input type="checkbox" id="theme-switcher">
+            <span class="switcher__slider-wrap"></span>
+          </label>
+          <button class="button switcher__theme-name">
+            Light
+          </button>
         </li>
         <li class="info__item">
           <button type="button" class="button info__volume"></button>
