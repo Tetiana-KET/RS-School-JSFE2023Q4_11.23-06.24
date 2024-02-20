0bb40efb4ffa82e47d054fa5dfee11b903d3ce84
tatiana <belangelphone@gmail.com>
2024-02-20 14:13:17 +0200

    refactor: add apiKey in appLoader.js

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super(process.env.API_URL, {
-            apiKey: process.env.API_KEY,
+            apiKey: '1f794da8a2eb4f83a68b9dc33fbeeff7',
         });
     }
 }
