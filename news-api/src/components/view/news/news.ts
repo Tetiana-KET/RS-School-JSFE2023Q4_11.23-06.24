4eafd548b09e5e4ec334ca1a82398a82e8c99edb
tatiana <belangelphone@gmail.com>
2024-02-20 21:19:20 +0200

    refactor: migrate News component to TypeScript Reviewed and adjusted code to resolve TypeScript compiler errors and warnings
    add interface to describe the structure of the source of a news item
    add interface to describe a news item (data deceived)
    edit a class News that encapsulates the functionality related to rendering news items
    edit draw method of the News class. It takes an array of NewsItem
    explicitly typed newsClone as DocumentFragment, to let TypeScript know that newsClone is of type DocumentFragment
    add Check for null when receiving elements with querySelector

---
+++ news-api/src/components/view/news/news.ts
@@ -0,0 +1,86 @@
+import './news.css';
+interface Source {
+    name: string;
+}
+
+interface NewsItem {
+    author: string | null;
+    source: Source;
+    publishedAt: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string | null;
+}
+
+class News {
+    draw(data: NewsItem[]) {
+        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = document.querySelector<HTMLTemplateElement>('#newsItemTemp');
+
+        if (newsItemTemp) {
+            news.forEach((item, idx) => {
+                const newsClone = newsItemTemp.content.cloneNode(true) as DocumentFragment;
+
+                if (idx % 2) {
+                    const newsItem = newsClone.querySelector('.news__item');
+                    if (newsItem) {
+                        newsItem.classList.add('alt');
+                    }
+                }
+
+                const newsMetaPhoto = newsClone.querySelector('.news__meta-photo');
+
+                if (newsMetaPhoto instanceof HTMLElement) {
+                    newsMetaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
+                }
+
+                const authorElement = newsClone.querySelector('.news__meta-author');
+                if (authorElement) {
+                    authorElement.textContent = item.author || item.source.name;
+                }
+
+                const dateElement = newsClone.querySelector('.news__meta-date');
+                if (dateElement) {
+                    dateElement.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
+                }
+
+                const titleElement = newsClone.querySelector('.news__description-title');
+                if (titleElement) {
+                    titleElement.textContent = item.title;
+                }
+
+                const sourceElement = newsClone.querySelector('.news__description-source');
+                if (sourceElement) {
+                    sourceElement.textContent = item.source.name;
+                }
+
+                const contentElement = newsClone.querySelector('.news__description-content');
+                if (contentElement) {
+                    contentElement.textContent = item.description;
+                }
+
+                const readMoreAnchor = newsClone.querySelector('.news__read-more a');
+                if (readMoreAnchor) {
+                    readMoreAnchor.setAttribute('href', item.url);
+                }
+
+                fragment.append(newsClone);
+            });
+
+            const newsContainer = document.querySelector('.news');
+            if (newsContainer) {
+                newsContainer.innerHTML = '';
+                newsContainer.appendChild(fragment);
+            } else {
+                throw new Error("Container '.news' not found");
+            }
+        } else {
+            throw new Error();
+        }
+    }
+}
+
+export default News;
