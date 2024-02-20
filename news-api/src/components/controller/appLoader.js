9a8d9aecd39f3fa0f3c8b48b34a2a0501105a7e2
tatiana <belangelphone@gmail.com>
2024-02-20 16:05:13 +0200

    refactor: set webpack, eslint and ts configs

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super(process.env.API_URL, {
-            apiKey: '1f794da8a2eb4f83a68b9dc33fbeeff7',
+            apiKey: process.env.API_KEY,
         });
     }
 }
