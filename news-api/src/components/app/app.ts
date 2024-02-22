973477c44d27700ea8481dab04d0fefd8a235f95
tatiana <belangelphone@gmail.com>
2024-02-22 18:31:53 +0200

    refactor: migrated app and index to TS   - edit import in index   - describe class properties   - add private modifier to properties

---
+++ news-api/src/components/app/app.ts
@@ -0,0 +1,25 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    private _controller: AppController;
+    private _view: AppView;
+
+    constructor() {
+        this._controller = new AppController();
+        this._view = new AppView();
+    }
+
+    start() {
+        const sourcesButtonsWrap = document.querySelector('.sources');
+        if (sourcesButtonsWrap) {
+            sourcesButtonsWrap.addEventListener('click', (e) =>
+                this._controller.getNews(e, (data) => this._view.drawNews(data))
+            );
+        }
+
+        this._controller.getSources((data) => this._view.drawSources(data));
+    }
+}
+
+export default App;
