9993ba5c2f27cf81990936997b450624865a48f7
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-03-10 22:31:41 +0200

    feat: Implement Personalized Greeting
    Implement Issue RSS-PZ-06: Display Personalized Greeting with User's Name from Local Storage

--- rss-puzzle/src/utils/commonUtils.ts
+++ rss-puzzle/src/utils/commonUtils.ts
@@ -13,3 +13,20 @@ export function storeUserData(firstName: string, surname: string): void {
   const userData = { firstName, surname };
   localStorage.setItem('userData', JSON.stringify(userData));
 }
+
+//get user name
+function getUserInfoFromLocalStorage(): { firstName: string; surname: string } | null {
+  const userData = localStorage.getItem('userData');
+  return userData ? JSON.parse(userData) : null;
+}
+
+// generate greeting
+export function generateGreeting(): string {
+  const userData = getUserInfoFromLocalStorage();
+  console.log(userData);
+  if (userData) {
+    return `Welcome, ${userData.firstName} ${userData.surname}!`;
+  } else {
+    return 'Welcome!';
+  }
+}
