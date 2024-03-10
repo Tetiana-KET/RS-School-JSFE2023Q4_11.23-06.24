ab003169b164a85ac7feb1ea007909af3c83ca28
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-03-10 09:59:08 +0200

    feat: Issue RSS-PZ-03: Save User's data in L.S. (#58)
    edit form validation
    add function storeUserData

--- rss-puzzle/src/utils/commonUtils.ts
+++ rss-puzzle/src/utils/commonUtils.ts
@@ -8,3 +8,8 @@ export function isNull<T>(value: Nullable<T>): value is NullLike {
 export function isNotNull<T>(value: Nullable<T>): value is NonNullable<T> {
   return value !== null && value !== undefined;
 }
+
+export function storeUserData(firstName: string, surname: string): void {
+  const userData = { firstName, surname };
+  localStorage.setItem('userData', JSON.stringify(userData));
+}
