6e08117a626efb98e1e29512e8fff0874958b47d
tatiana <belangelphone@gmail.com>
2024-03-10 17:06:58 +0200

    feat: Design Start Screen

--- rss-puzzle/src/main.ts
+++ rss-puzzle/src/main.ts
@@ -3,11 +3,13 @@ import { LoginPage } from './pages';
 //// import viteLogo from '/vite.svg'
 import './normalize.css';
 import './style.css';
+import { StartScreen } from './pages/startScreen/StartScreen';
 
-const btn = new Button();
-const ddd = new Component({ tagName: 'h3', text: 'Hello world' });
-ddd.append(btn);
+// const btn = new Button();
+// const ddd = new Component({ tagName: 'h3', text: 'Hello world' });
+// ddd.append(btn);
 
-document.body.append(ddd.getNode());
+// document.body.append(ddd.getNode());
 // создание и вставка формы на страницу
+document.body.append(new StartScreen().getNode());
 //document.body.append(new LoginPage().getNode());
