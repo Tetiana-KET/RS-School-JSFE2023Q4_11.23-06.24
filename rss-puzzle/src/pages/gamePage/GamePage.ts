90ada4d88f6e88585fb619ee0cb75245b3a8ed4a
tatiana <belangelphone@gmail.com>
2024-03-12 10:05:12 +0200

    refactor: improve design

--- rss-puzzle/src/pages/gamePage/GamePage.ts
+++ rss-puzzle/src/pages/gamePage/GamePage.ts
@@ -1,28 +1,42 @@
 import { Component } from '../../components';
-import { GameFooter } from '../../components/gameFooter/GameFooter';
+import { Footer } from '../../components/footer/Footer';
+import { GameButtonsBlock } from '../../components/GameButtonsBlock/GameButtonsBlock';
 import { GameHeader } from '../../components/gameHeader/GameHeader';
 import classes from './GamePage.module.css';
+import bg from '../../assets/bg.jpg';
 
 export class GamePage extends Component {
+  private gamePageContainer: Component;
   private header: Component;
   private mainContent: Component<HTMLDivElement>;
   private gameWrap: Component<HTMLDivElement>;
   private gameSourceDataBlock: Component<HTMLDivElement>;
+  private gameButtonsBlock: Component;
   private footer: Component;
 
   constructor() {
-    super({ tagName: 'div', classNames: [classes.gamePageContainer] });
+    super({ tagName: 'div', classNames: [classes.gamePageBg] });
+    this.getNode().style.backgroundImage = `url(${bg})`;
+    this.getNode().style.backgroundSize = 'cover';
+    this.getNode().style.backgroundRepeat = 'no-repeat';
+    this.getNode().style.backgroundPosition = 'center';
+    // gamePageContainer
+    this.gamePageContainer = new Component({
+      tagName: 'div',
+      classNames: [classes.gamePageContainer],
+    });
+    this.append(this.gamePageContainer);
 
     // header
     this.header = new GameHeader();
-    this.append(this.header);
+    this.gamePageContainer.append(this.header);
 
     // main content wrapper
     this.mainContent = new Component({
-      tagName: 'div',
+      tagName: 'main',
       classNames: [classes.mainContentWrapper],
     });
-    this.append(this.mainContent);
+    this.gamePageContainer.append(this.mainContent);
 
     // game wrapper
     this.gameWrap = new Component({
@@ -38,8 +52,12 @@ export class GamePage extends Component {
     });
     this.mainContent.append(this.gameSourceDataBlock);
 
+    //buttons
+    this.gameButtonsBlock = new GameButtonsBlock();
+    this.mainContent.append(this.gameButtonsBlock);
+
     // Footer
-    this.footer = new GameFooter();
-    this.append(this.footer);
+    this.footer = new Footer();
+    this.gamePageContainer.append(this.footer);
   }
 }
