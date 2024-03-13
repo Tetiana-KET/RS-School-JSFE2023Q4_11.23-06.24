2872dffe7cf34b257aabc5f52ca02a52a8ac5818
tatiana <belangelphone@gmail.com>
2024-03-13 14:51:13 +0200

    feat: add continue button to layout

--- rss-puzzle/src/components/GameButtonsBlock/GameButtonsBlock.ts
+++ rss-puzzle/src/components/GameButtonsBlock/GameButtonsBlock.ts
@@ -4,17 +4,25 @@ import classes from './GameButtonsBlock.module.css';
 
 export class GameButtonsBlock extends Component {
   private solutionButton: Component<HTMLButtonElement>;
-
+  private continueButton: Component<HTMLButtonElement>;
   constructor() {
     super({ tagName: 'div', classNames: [classes.gameButtonsBlock] });
 
-    //buttons
+    //Solution button
     this.solutionButton = new Component({
       tagName: 'button',
-      text: `I don't know`,
-      classNames: [classes.solutionButton],
+      text: `Solution`,
+      classNames: [classes.button, classes.solutionButton],
     });
     this.append(this.solutionButton);
+    //continue button
+    this.continueButton = new Component<HTMLButtonElement>({
+      tagName: 'button',
+      classNames: [classes.button, classes.continueButton],
+      text: 'Continue',
+      attributes: { type: 'button', disabled: true },
+    });
+    this.append(this.continueButton);
     // Event listener for logout button
     this.solutionButton.getNode().addEventListener('click', this.handleButtonClick.bind(this));
   }
