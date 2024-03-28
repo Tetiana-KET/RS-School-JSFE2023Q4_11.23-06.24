887afdcf07828611affa3c91105befa01eafb6d0
tatiana <belangelphone@gmail.com>
2024-03-28 17:17:00 +0200

    refactor: replace garage components templates into separate file

--- async-race/src/app/views/garage/Garage.ts
+++ async-race/src/app/views/garage/Garage.ts
@@ -3,6 +3,7 @@ import { Component } from '../../components/Component';
 import { createCar, deleteCar, updateCar } from '../../utils/InteractionAPI';
 import { createCarsInGarage, updateGarageTitle } from '../../utils/RenderingUI';
 import Car from '../car/Car';
+import { createFormWrapper, createGarageTitle } from './garage.utils';
 
 export default class GarageView extends Component {
   private formWrap: Component<HTMLDivElement>;
@@ -22,6 +23,7 @@ export default class GarageView extends Component {
     this.paginationWrap.getNode().innerHTML = this.createPagination();
     this.appendElements(this.formWrap, this.titleWrap, this.garageRaceContainer, this.paginationWrap);
     this.createGarageView(this.garageRaceContainer.getNode(), this.currentPage, this.CARS_LIMIT);
+    this.generateBtnAddListener();
   }
 
   private createBtnAddListener(): void {
@@ -39,6 +41,11 @@ export default class GarageView extends Component {
   private updateBtnAddListener(): void {
     // UPDATE CAR
     this.formWrap.getNode().querySelector(`.${classes.updateBtn}`)?.addEventListener('click', this.updateBtnClickHandler.bind(this));
+  }
+
+  private generateBtnAddListener(): void {
+    const generateBtn = this.formWrap.getNode().querySelector(`.${classes.generateBtn}`);
+    generateBtn?.addEventListener('click', this.generateBtnClickHandler);
   }
 
   private appendElements(
@@ -140,39 +147,11 @@ export default class GarageView extends Component {
   }
 
   private createFormWrapper(): string {
-    return `
-      <div class="${classes.formWrapper}">
-        <div class="${classes.form}">
-          <form action="" class="${classes.carForm}" id="create-car">
-            <input class="${classes.formNameInput}" type="text" id="create-car-name" name="car-name" minlength="3" placeholder="Car name (min 3 symbols)"><br>
-            <input type="color" id="create-car-color" name="car-color" value="#ffffff">
-            <button type="button" class="${classes.createBtn} ${classes.button}" id="createBtn" disabled>Create</button>
-          </form>
-        </div>
-        <div class="${classes.form}">
-          <form action="" class="${classes.carForm}" id="update-car">
-            <input class="${classes.formNameInput}" type="text" id="update-car-name" name="car-name" minlength="3" placeholder="Select car with select button"><br>
-            <input type="color" id="update-car-color" name="car-color" value="#ffffff">
-            <button type="button" class="${classes.updateBtn} ${classes.button}" disabled id="updateBtn">Update</button>
-          </form>
-        </div>
-      </div>
-      <div class="${classes.garageMenuButtons}">
-        <button class="${classes.raceBtn} ${classes.button}">Start Race</button>
-        <button class="${classes.resetBtn} ${classes.button}" disabled>Reset</button>
-        <button class="${classes.generateBtn} ${classes.button}">Generate cars</button>
-      </div>`;
+    return createFormWrapper(classes);
   }
 
   private createGarageTitle(page: number): string {
-    return `
-        <div class="${classes.carsCount}" id="carsCount">
-          <h2></h2>
-        </div>
-        <div class="${classes.garagePageNumber}">
-          <h3 class="${classes.pageNumber}" data-page="${page}">Page: ( № ${page} )</h3>
-        </div>
-    `;
+    return createGarageTitle(page, classes);
   }
 
   private createPagination(): string {
---
+++ async-race/src/app/views/garage/garage.utils.ts
@@ -0,0 +1,35 @@
+export function createFormWrapper(classes: CSSModuleClasses): string {
+  return `
+      <div class="${classes.formWrapper}">
+        <div class="${classes.form}">
+          <form action="" class="${classes.carForm}" id="create-car">
+            <input class="${classes.formNameInput}" type="text" id="create-car-name" name="car-name" minlength="3" placeholder="Car name (min 3 symbols)"><br>
+            <input type="color" id="create-car-color" name="car-color" value="#ffffff">
+            <button type="button" class="${classes.createBtn} ${classes.button}" id="createBtn" disabled>Create</button>
+          </form>
+        </div>
+        <div class="${classes.form}">
+          <form action="" class="${classes.carForm}" id="update-car">
+            <input class="${classes.formNameInput}" type="text" id="update-car-name" name="car-name" minlength="3" placeholder="Select car with select button"><br>
+            <input type="color" id="update-car-color" name="car-color" value="#ffffff">
+            <button type="button" class="${classes.updateBtn} ${classes.button}" disabled id="updateBtn">Update</button>
+          </form>
+        </div>
+      </div>
+      <div class="${classes.garageMenuButtons}">
+        <button class="${classes.raceBtn} ${classes.button}">Start Race</button>
+        <button class="${classes.resetBtn} ${classes.button}" disabled>Reset</button>
+        <button class="${classes.generateBtn} ${classes.button}">Generate cars</button>
+      </div>`;
+}
+
+export function createGarageTitle(page: number, classes: CSSModuleClasses): string {
+  return `
+        <div class="${classes.carsCount}" id="carsCount">
+          <h2></h2>
+        </div>
+        <div class="${classes.garagePageNumber}">
+          <h3 class="${classes.pageNumber}" data-page="${page}">Page: ( № ${page} )</h3>
+        </div>
+    `;
+}
