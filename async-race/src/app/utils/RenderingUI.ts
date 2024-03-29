2d976df781b2ffb5f34d4b1b3f34a84c351ba30d
tatiana <belangelphone@gmail.com>
2024-03-29 15:19:48 +0200

    feat: add logic to switch pages and toggle buttons

--- async-race/src/app/utils/RenderingUI.ts
+++ async-race/src/app/utils/RenderingUI.ts
@@ -2,10 +2,14 @@ import { GarageInterface } from '../interfaces/car.interface';
 
 import { getCars } from './InteractionAPI';
 
-export function updateGarageTitle(carsCount: number): void {
+export function updateGarageTitle(carsCount: number, currentPage: number, lastPage: number = 1): void {
   const titleWrapper = document.querySelector(`#carsCount h2`);
   if (titleWrapper) {
     titleWrapper.innerHTML = `<h2>Cars in Garage: ( ${carsCount} )</h2>`;
+  }
+  const pageNumberWrapper = document.querySelector(`#pageNumber`);
+  if (pageNumberWrapper) {
+    pageNumberWrapper.textContent = `Page: ( № ${currentPage} / ${lastPage})`;
   }
 }
 
@@ -14,4 +18,27 @@ export async function createCarsInGarage(page: number, limit: number): Promise<[
   const carsInGarage = await cars;
   const totalCars = total;
   return [carsInGarage, totalCars];
+}
+
+export function togglePaginationBtnsState(currentPage: number, lastPage: number): void {
+  const firstPageBtn = document.querySelector('#firstPageBtn') as HTMLButtonElement;
+  const prevBtn = document.querySelector('#prevBtn') as HTMLButtonElement;
+  const nextBtn = document.querySelector('#nextBtn') as HTMLButtonElement;
+  const lastPageBtn = document.querySelector('#lastPageBtn') as HTMLButtonElement;
+
+  if (currentPage > 1) {
+    firstPageBtn.removeAttribute('disabled');
+    prevBtn.removeAttribute('disabled');
+  } else {
+    firstPageBtn.setAttribute('disabled', 'true');
+    prevBtn.setAttribute('disabled', 'true');
+  }
+
+  if (currentPage < lastPage) {
+    lastPageBtn.removeAttribute('disabled');
+    nextBtn.removeAttribute('disabled');
+  } else {
+    lastPageBtn.setAttribute('disabled', 'true');
+    nextBtn.setAttribute('disabled', 'true');
+  }
 }
