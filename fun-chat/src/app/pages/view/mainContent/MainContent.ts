31ef424c1e71abb60a41ba3f3888f28220960c28
tatiana <belangelphone@gmail.com>
2024-04-18 10:44:23 +0300

    refactor: add check wether user is logged

--- fun-chat/src/app/pages/view/mainContent/MainContent.ts
+++ fun-chat/src/app/pages/view/mainContent/MainContent.ts
@@ -1,12 +1,10 @@
-// import { Controller } from '../../controllers/Controller';
 import { AboutPage } from '../../aboutPage/AboutPage';
 import { ChatPage } from '../../chatPage/ChatPage';
 import { LoginPage } from '../../loginPage/LoginPage';
 import { Router } from '../../Router';
 import { Component } from '../../../components/Component';
 import classes from './MainContent.module.css';
-// import { isLoggedFromSessionStorage } from '../../../utils/commonUtils';
-// import { eventBus } from '../../../utils/eventBus';
+import { isLoggedFromSessionStorage } from '../../../utils/commonUtils';
 
 export class MainContent extends Component<'main'> {
   private router: Router;
@@ -23,32 +21,18 @@ export class MainContent extends Component<'main'> {
 
     this.router = new Router(this.setPageContent.bind(this));
     console.log(this.router);
-    // window.addEventListener('reload', () => {
-    //   this.setPageContent();
-    // });
-    // eventBus.subscribe('userLoggedOut', () => {
-    //   this.drawLoginPage();
-    // });
+    window.addEventListener('unload', () => {
+      this.setPageContent();
+    });
   }
 
   private setPageContent(): void {
     const location = window.location.hash;
-    // if (isLoggedFromSessionStorage()) {
-    //   if (location === '#chat') {
-    //     this.drawChatPage();
-    //   } else if (location === '#about') {
-    //     this.drawAboutPage();
-    //   }
-    // } else {
-    //   if (location === '') {
-    //     this.drawLoginPage();
-    //   } else if (location === '#chat') {
-    //     window.location.hash === '';
-    //     this.drawLoginPage();
-    //   } else if (location === '#about') {
-    //     this.drawAboutPage();
-    //   }
-    // }
+    if (!isLoggedFromSessionStorage()) {
+      if (location === '#chat') {
+        window.location.hash = '';
+      }
+    }
 
     if (location === '') {
       this.drawLoginPage();
